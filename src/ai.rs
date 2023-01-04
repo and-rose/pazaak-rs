387779a379ddd5a09a4c246527b87f56a5235a29
Kevin San Diego <ksandiego97@gmail.com>
2023-01-04 22:29:36 +1000

    AI Stuff

--- src/ai.rs
+++ src/ai.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, rc::Weak};
 
 use rand::seq::SliceRandom;
 
@@ -13,8 +13,12 @@ pub fn get_best_move(pazaak_match: &Match, simulations: i8) {
     let ai_hand = &pazaak_match.players[1].hand;
 }
 
-fn expand() {
+fn select() {
     
+}
+
+fn expand() {
+
 }
 
 fn simulate() {
@@ -25,13 +29,40 @@ fn back_propagate() {
 
 }
 
+fn best_ucb_score(node: Node) {
+    let mut best_node = node;
+    let mut max_ucb = 0.0;
+
+    for child in node.children {
+        let child_ucb = calculate_ucb_score(child.score, child.visit, child.parent.upgrade().unwrap().visit);
+
+        if child_ucb >= max_ucb {
+            max_ucb = child_ucb;
+            best_node = child;
+        }        
+    }
+    return best_node;
+}
+
+fn calculate_ucb_score(win_score: f32, visit_score:f32, parent_visit_score: f32) -> f32 {
+    if visit_score < 1.0 {
+        return f32::INFINITY;
+    }
+
+    let constant = 1.4;
+    let win_ratio = win_score/ visit_score;
+    let exploration = (parent_visit_score.log10() / visit_score).sqrt();
+    return win_ratio + constant * exploration;
+}
+
 struct Node {
     pub game: Game,
     pub hand: Hand,
-    pub parent: HashMap<Action, Node>, // Action also needs to track which card if a card is played
+    pub action: Action,
+    pub parent: Weak<Node>, //TODO: Weak reference to parent
     pub children: Vec<Node>,
-    pub visit: i8, // denominator
-    pub score: i8, // numerator
+    pub visit: f32,
+    pub score: f32,
     // In games where draws are possible,
     // a draw causes the numerator for both
     // black and white to be incremented by 0.5 and the denominator by 1
